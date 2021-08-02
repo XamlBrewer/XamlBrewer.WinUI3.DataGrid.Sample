9d818c456e4a44172155f5e1b4715b2dab8e5f26
Diederik Krols <diederik_krols@hotmail.com>
2021-08-02 21:21:43 +0200

    Template column

---
+++ XamlBrewer.WinUI3.DataGrid.Sample/XamlBrewer.WinUI3.DataGrid.Sample/Services/SliderValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace XamlBrewer.WinUI3.DataGrid.Sample.Services
+{
+    public class SliderValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+
+            return String.Format($"{value} m");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
