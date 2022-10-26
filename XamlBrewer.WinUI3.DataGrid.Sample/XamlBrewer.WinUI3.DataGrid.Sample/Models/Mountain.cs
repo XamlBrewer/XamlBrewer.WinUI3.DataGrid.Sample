a6f507e9f30992f1174910b38068614956e069ce
Diederik Krols <diederik_krols@hotmail.com>
2022-10-26 20:26:07 +0200

    Upgrade to WindowsAppSDK.

--- XamlBrewer.WinUI3.DataGrid.Sample/XamlBrewer.WinUI3.DataGrid.Sample/Models/Mountain.cs
+++ XamlBrewer.WinUI3.DataGrid.Sample/XamlBrewer.WinUI3.DataGrid.Sample/Models/Mountain.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace XamlBrewer.WinUI3.DataGrid.Sample.Models
